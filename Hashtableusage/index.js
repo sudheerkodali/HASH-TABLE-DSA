7ef2504ff5066eb14581770f8d801b788a73e080
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2024-04-04 11:17:24 +0530

    Update index.js

--- Hashtableusage/index.js
+++ Hashtableusage/index.js
@@ -1,1 +1,20 @@
 
+const map = new MAP();
+
+map.set( 'Apple IPhone 11', 50000)
+map.set('Samsung Fold', 10000)
+
+console.log(map.get('Samsung Fold'));
+//map.delete('samsung fold');
+
+//console.log('Size of the method = ${map.size}')
+//map.delete('Apple IPhone 11');
+
+//console.log('has IPhone = ' + map.has('Apple IPhone 11'))
+
+map.forEach((val, key) => {
+   console.log('key = ${key} and value = ${val}')
+  })
+
+    
+
